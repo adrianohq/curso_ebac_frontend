d57c96099f16edc6757fa26021ab85cf748ac722
Adriano Henrique <116513971+adrianohq@users.noreply.github.com>
2024-03-21 20:08:42 -0300

    Upload do exercício

---
+++ exercicio.ts
@@ -0,0 +1,10 @@
+const multiplicar = (numeroX: number, numeroY: number): number => {
+    return numeroX * numeroY;
+}
+
+const saudacao =  (nome: string): string => {
+    return "Olá " + nome;
+}
+
+console.log(multiplicar(5,3));
+console.log(saudacao('Maria'));
